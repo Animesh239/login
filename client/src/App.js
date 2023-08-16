7ef5409ea844cb7ae0d53f3b4dbf198cd2ddd1a8
Animesh239 <animeshbarik239@gmail.com>
2023-08-16 19:26:42 +0530

    react mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
+import * as React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import SignIn from "./auth/signin";
+import SignUp from "./auth/signup";
+import Home from "./pages/home";
 
-import './App.css';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default router;
