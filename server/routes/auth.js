bd21f96437f3bb873cf8cc622bad91054e209518
Animesh239 <animeshbarik239@gmail.com>
2023-08-18 00:44:10 +0530

    user authenticated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,33 +1,41 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-let users = [] ;
+let users = [];
 
-router.get('/' , (req,res,next)=>{
-    res.json({ users: users })
-})
+router.get("/", (req, res, next) => {
+  res.json({ users: users });
+});
 
-router.post('/signup' , (req,res,next)=>{
-    const newUser = { id : new Date().toISOString() , name : req.body.name , email : req.body.email , password : req.body.password }
-    users.push(newUser)
-    res.json({ message : "new User added" , userDetails : newUser})
-})
+router.post("/signup", (req, res, next) => {
+  const newUser = {
+    id: new Date().toISOString(),
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  users.push(newUser);
+  res.json({ message: "new User added", userDetails: newUser });
+});
 
-router.post('/signin', (req,res,next)=>{
+router.post("/signin", async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const userIndex = users.findIndex((user)=>{
-        // console.log("index fteched")
-        return user.email === email 
-    })
-    // if(userIndex == -1){
-    //    return res.json({ message : " invalid email "})
-    // }
-    
-    res.json({ message : "user fetched" , user : users[userIndex] })
-})
-
-
+  
+    const user = await users.find((user) => {
+      return user.email === email 
+    });
+  
+    if (!user) {
+            return res.json({ message: "email does not exist" });
+          }
+          if (user.password !== password) {
+            return res.json({ message: "invalid password" });
+          }
+  
+    console.log("user loggedin successfully");
+    return res.json({ messsage: "user authenticated", user: user });
+  });
 
 module.exports = router;
