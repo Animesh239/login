f035b1769cda91db5e67a4a457f265079b1e5f4c
Animesh239 <animeshbarik239@gmail.com>
2023-08-18 16:17:38 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+    res.header.set('Access-Control-Allow-Origin' , '*');
+    res.header.set('Access-Control-Allow-Methods' , 'GET, POST, PUT, DELETE');
+    res.header.set('Access-Control-Allow-Headers' , 'Content-Type');
+    next();
+  })
+
 app.use(authRoutes) ;
 
 app.listen(8000)
